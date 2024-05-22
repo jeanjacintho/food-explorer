5bcca73a0748d4cd843ef5d3967ca75b0be7be05
jeanjacintho <jeanjacintho@yahoo.com.br>
2024-05-22 01:30:43 -0300

    remove cors origin

--- API/src/server.js
+++ API/src/server.js
@@ -15,7 +15,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true
 }));
 app.use(routes);
